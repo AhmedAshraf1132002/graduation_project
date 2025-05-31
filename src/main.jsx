a245265e8ab56f1678162ad33d2114a377b29b74
AhmedAshraf1132002 <ahmedashraf200261@gmail.com>
2025-06-01 02:44:48 +0300

    Fix Vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; 
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,10 +10,8 @@ import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter> 
       <HelmetProvider>
         <App />
       </HelmetProvider>
-    </BrowserRouter>
   </StrictMode>
 );
