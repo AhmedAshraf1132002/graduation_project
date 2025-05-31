2929590e9dae2b0c6d4cc6f1b8e92fffe9557a57
AhmedAshraf1132002 <ahmedashraf200261@gmail.com>
2025-06-01 02:29:35 +0300

    Fix: Add BrowserRouter for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom"; 
 import App from "./App.jsx";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { HelmetProvider } from "react-helmet-async";
 import "./i18n";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <BrowserRouter> 
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
   </StrictMode>
 );
